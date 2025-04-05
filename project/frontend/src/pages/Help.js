fa6161a6eaebc099b90f6e22994328ffa0901acb
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-04-05 23:09:26 +0300

    Create Help.js

---
+++ project/frontend/src/pages/Help.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Help = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Help Center</h1>
+      {/* Add your help page content */}
+    </div>
+  );
+};
+
+export default Help; 
